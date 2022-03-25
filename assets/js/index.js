27200f6f38cc843ded799d034f539530b538aeb5
AnisaFitrianidaLP <2010631170003@student.unsika.ac.id>
2022-03-25 22:50:38 +0700

    Saya sudah menambahkan website UTS

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+var nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 100) {
+        nav.classList.add('bg-light', 'shadow');
+    } else {
+        nav.classList.remove('bg-light', 'shadow' );
+    }
+});
+
+
+// Scroll Top
+function scrollToTop(){
+    window.scrollTo(0,0);
+}
